47506382df766606850d8ba481a8b91b76d8274f
snoop244 <snewport@korio.ca>
2018-08-26 11:04:40 -0400

    Draft create

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+public class Program
+{
+    public static void Main()
+    {
+        var config = new Dictionary<string, object> 
+        { 
+            { "bootstrap.servers", "localhost:9092" } 
+        };
+
+        using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+        {
+            var dr = producer.ProduceAsync("my-topic", null, "test message text").Result;
+            Console.WriteLine($"Delivered '{dr.Value}' to: {dr.TopicPartitionOffset}");
+        }
+    }
+}
